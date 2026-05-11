10de7a05c23769ee503efa1d2858fda58a9dec6e
caibirana2-eng <caibirana2@stu.otc.school.nz>
2026-05-11 13:48:54 +1200

    aaaaaaa

--- login.py
+++ login.py
@@ -1,12 +1,22 @@
 from flask import Flask, render_template, request
 import sqlite3
 
+con = sqlite3.connect('accounts.db')
+cur = con.cursor()
+cur.execute('SELECT * FROM accounts')
+accounts = cur.fetchall()
+con.close()
+
+print(accounts)
+
 app = Flask(__name__)
-print(app)
 
 @app.route("/", methods=['GET', 'POST'])
 def login():
-    bozo = "HI"
-    return render_template('login.html', bozobozo=bozo)
+    if request.method == "POST":
+        if "usernametype" in request.form or "passwordtype" in request.form or "confirmlogin" in request.form:
+            print(request.form.get("usernametype"))
+            print(request.form.get("passwordtype"))
+    return render_template('login.html')
 
 app.run(host="127.0.0.1", port=5000, debug=True)
