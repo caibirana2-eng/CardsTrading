fa0994116a3c2369eb121173e1632e98c4a9a342
caibirana2-eng <caibirana2@stu.otc.school.nz>
2026-05-28 13:26:40 +1200

    Trying to reformat the program's flow so that the same pages are used for signup email and recovery email, as well as account creating and changing account details after recovery. Also made it so that the secret key for sessions is randomized each time the program is started.

--- routes.py
+++ routes.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, redirect, url_for, session, request
 import sqlite3
+import secrets
 
 con = sqlite3.connect('accounts.db')
 cur = con.cursor()
@@ -9,13 +10,11 @@ con.close()
 
 app = Flask(__name__)
 
-emailinput = ""
-
-session['user_logged_in'] = ""
+app.secret_key = secrets.token_hex(32)
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    if session.get['user_logged_in'] == "":
+    if not session.get('user_logged_in'):
         return redirect(url_for("login"))
     return render_template('index.html')
 
@@ -31,6 +30,7 @@ def login():
             for i in accounts:
                 if infoinput[0] == i[0] and infoinput[1] == i[1]:
                     session['user_logged_in'] = infoinput[0]
+                    print(session.get('user_logged_in'))
                     return redirect(url_for("index"))
             error = "Incorrect username or password!"
     return render_template('login.html', errormessage=error)
@@ -60,7 +60,7 @@ def signupcode():
     error = ""
     if request.method == "POST":
         if "confirmcodeemail" in request.form:
-            if request.form.get('codeemail') == session.get['emailcode']:
+            if request.form.get('codeemail') == session.get('emailcode'):
                 return redirect(url_for("makeaccount"))
             else:
                 error = "Incorrect Code!"
@@ -71,6 +71,26 @@ def signupcode():
 def makeaccount():
     error = ""
     return render_template('makeaccount.html', errormessage=error)
+
+@app.route("/forgotpass", methods=['GET', 'POST'])
+def forgotpass():
+    global accounts
+    error = ""
+    if request.method == "POST":
+        if "confirmrecoveryemail" in request.form:
+
+            # code to send email and check if valid, then generate a random teporary code with would be here
+            # using emailtype == "errorpls" or "" just to show the error message working
+            # Using a set code since I can't email a random temp code
+            if request.form.get("emailtype") == "errorpls" or request.form.get("emailtype") == "":
+                error = "Entered invalid or taken email!"
+            else:
+                
+
+                session['emailcode'] = "654321" #supposed to be made with random.randint and made temporary with session flask function
+                #followed by send code to given email
+                return redirect(url_for("forgotpasscode"))            
+    return render_template('signup.html', errormessage=error)
 
 
 app.run(host="127.0.0.1", port=5000, debug=True)
