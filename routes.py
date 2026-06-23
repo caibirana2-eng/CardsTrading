9379db641bd4dcb1bbe398439626488a794cbb0f
caibirana2-eng <caibirana2@stu.otc.school.nz>
2026-06-24 10:00:22 +1200

    did more work on card search

--- routes.py
+++ routes.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template, redirect, url_for, session, request
 import sqlite3, secrets, os
 
-con = sqlite3.connect('accounts.db', check_same_thread=False)
-cur = con.cursor()
+conaccounts = sqlite3.connect('accounts.db', check_same_thread=False)
+accountscur = conaccounts.cursor()
+
+concards = sqlite3.connect('cards.db', check_same_thread=False)
+cardsearchcur = concards.cursor()
 
 app = Flask(__name__)
 
@@ -25,7 +28,14 @@ def index():
 def cardsearch():
     if not session.get('user_logged_in'):
         return redirect(url_for("login"))
-    return render_template('cardsearch.html')
+    cardsearchcur.execute("SELECT cardimg FROM cards")
+    showncards = cardsearchcur.fetchall()
+    cardsearchcur.execute("SELECT releasedate, avgprice, inforecency, fromset FROM cards")
+    categories = cardsearchcur.fetchall()
+    print(categories)
+    if request.method == "POST":
+        print(request.form.get("card"))
+    return render_template('cardsearch.html', showncards=showncards, categories=categories)
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
@@ -33,11 +43,10 @@ def login():
     if request.method == "POST":
         if "confirmlogin" in request.form:
             infoinput = [request.form.get("usernametype"), request.form.get("passwordtype")]
-            cur.execute('SELECT * FROM accounts WHERE username = ? AND password = ?', (infoinput[0], infoinput[1]))
-            data = cur.fetchone()
+            accountscur.execute('SELECT * FROM accounts WHERE username = ? AND password = ?', (infoinput[0], infoinput[1]))
+            data = accountscur.fetchone()
             if data != None:
                 session['user_logged_in'] = infoinput[0]
-                print(session.get('user_logged_in'))
                 return redirect(url_for("index"))
             error = "Incorrect username or password! (Case sensitive)"
     return render_template('login.html', errormessage=error)
@@ -52,8 +61,8 @@ def signup():
             # using emailtype == "erorrpls" as a placeholder for invalid emails
             # Using a set code since I can't email a random temp code
             givenemail = request.form.get("emailtype")
-            cur.execute('SELECT * FROM accounts WHERE email = ?', (givenemail,))
-            data = cur.fetchone()
+            accountscur.execute('SELECT * FROM accounts WHERE email = ?', (givenemail,))
+            data = accountscur.fetchone()
             if request.form.get("emailtype") == "errorpls" or request.form.get("emailtype") == "" or data != None:
                 error = "Entered invalid or taken email!"
             else:
@@ -69,8 +78,8 @@ def makeaccount():
     #Checks if a username is already linked to the given forgotpass email   
     error = ""
     givenemail = session.get('givenemail')
-    cur.execute('SELECT username FROM accounts WHERE email = ?', (givenemail,))
-    pastusername = cur.fetchone()
+    accountscur.execute('SELECT username FROM accounts WHERE email = ?', (givenemail,))
+    pastusername = accountscur.fetchone()
     if pastusername == None:
         cleanpastusername = ""
     else:
@@ -83,8 +92,8 @@ def makeaccount():
         loweredcreateusername = createusername.lower()
 
         #Creates a collection of every username in the database lowered
-        cur.execute('SELECT LOWER(username) FROM accounts')
-        data = cur.fetchall()
+        accountscur.execute('SELECT LOWER(username) FROM accounts')
+        data = accountscur.fetchall()
         cleandata = (account[0] for account in data)
 
         #Checks if the username input is either the past username or is not taken 
@@ -103,8 +112,8 @@ def makeaccount():
                     elif len(createpassword) < 10:
                         error = "Password must be at least 10 characters long"
                     else:
-                        cur.execute('INSERT INTO accounts (username, password, email) VALUES (?, ?, ?)', (createusername, createpassword, givenemail,))
-                        con.commit()
+                        accountscur.execute('INSERT INTO accounts (username, password, email) VALUES (?, ?, ?)', (createusername, createpassword, givenemail,))
+                        conaccounts.commit()
                         return redirect(url_for("login"))
                 else:                
                     if not 3 <= len(createusername) <= 20:
@@ -114,8 +123,8 @@ def makeaccount():
                     elif len(createpassword) < 10:
                         error = "Password must be at least 10 characters long"
                     else:
-                        cur.execute('UPDATE accounts SET username =?, password = ? WHERE email = ?', (createusername, createpassword, givenemail,))
-                        con.commit()
+                        accountscur.execute('UPDATE accounts SET username =?, password = ? WHERE email = ?', (createusername, createpassword, givenemail,))
+                        conaccounts.commit()
                         return redirect(url_for("login"))
                     
                     #added pastusername=cleanpastusername for recognition rather than recall   
@@ -129,8 +138,8 @@ def forgotpass():
 
             # Same case here as in /signup route
             givenemail = request.form.get("emailtypeforgot")
-            cur.execute('SELECT * FROM accounts WHERE email = ?', (givenemail,))
-            data = cur.fetchone()
+            accountscur.execute('SELECT * FROM accounts WHERE email = ?', (givenemail,))
+            data = accountscur.fetchone()
             if request.form.get("emailtype") == "errorpls" or request.form.get("emailtype") == "" or data == None:
                 error = "Entered invalid email!"
             else:
